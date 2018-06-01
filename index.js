25a769377b1eacbc836c671227da2e53ca71ec22
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-06-01 23:03:54 +0530

    testing

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ const {
   // Register handlers for Dialogflow intents
    
   app.intent('Default Welcome Intent', conv => {
-    conv.ask('Hi, how is it going?')
+    conv.ask('Hi, how is it going?\nhhh hhhhhghgh jjjh\n')
     conv.ask(`Here's a picture of a cat`)
     conv.ask(new Image({
       url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
       alt: 'A cat',
     }))
-    conv.ask('Hello')
-    conv.ask('How are you')
+   
   })
    
   // Intent in Dialogflow called `Goodbye`

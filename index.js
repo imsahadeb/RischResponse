ea4a52af4a740fb1143efccbbf416b93542d03e9
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-06-02 09:09:43 +0530

    testig

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ const {
    
   app.intent('Default Welcome Intent', conv => {
     getTrainList(conv,pass);
+    function getTrainList(conv,pass){
+      url='https://api.railwayapi.com/v2/between/source/ncb/dest/bhp/date/01-06-2018/apikey/ye1rpmx0tk/'
+      fromApi.callTheRailwayApi(url,(response)=>{
+        let getJsonRsponse=JSON.parse(response);
+        console.log(getJsonRsponse);
+        trainName =getJsonRsponse.trains[0].name;
+        
+      })
+      pass(conv.ask(trainName));
+    }
+  
    
    
   })
@@ -27,15 +38,5 @@ const {
     conv.ask(`I didn't understand. Can you tell me something else?`)
   })
 
-  function getTrainList(conv,pass){
-    url='https://api.railwayapi.com/v2/between/source/ncb/dest/bhp/date/01-06-2018/apikey/ye1rpmx0tk/'
-    fromApi.callTheRailwayApi(url,(response)=>{
-      let getJsonRsponse=JSON.parse(response);
-      console.log(getJsonRsponse);
-      trainName =getJsonRsponse.trains[0].name;
-      
-    })
-    pass(conv.ask(trainName));
-  }
-
+  
   express().use(bodyParser.json(), app).listen(process.env.PORT || 80)
